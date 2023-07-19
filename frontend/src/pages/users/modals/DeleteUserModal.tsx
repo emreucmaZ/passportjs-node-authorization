371fb143f990920bb1cb426a51b8f5a842297852
Emre UÇMAZ <yemreucmzz@gmail.com>
2023-07-19 11:25:37 +0300

    Add enter on delete user to modal

--- frontend/src/pages/users/modals/DeleteUserModal.tsx
+++ frontend/src/pages/users/modals/DeleteUserModal.tsx
@@ -1,36 +1,50 @@
+import { Box, Button, Modal } from "@mui/material";
+import React from "react";
 import {
-    Box,
-    Button,
-    Modal
-} from "@mui/material";
-import React, { useState } from "react";
-import { ICreateUpdateUserForm, ICreateUserModalProps, IDeleteUserModalProps } from "./interfaces";
+  IDeleteUserModalProps,
+} from "./interfaces";
 import { modalBoxStyle } from "@/variables";
-import { createUser, deleteUser } from "./functions";
-import { IRole } from "@/redux/interfaces/role/IRole";
-import { useFormik } from "formik";
-import * as Yup from "yup"
+import { deleteUser } from "./functions";
 
-function DeleteUserModal({ isVisible, handleClose, state, userId,router}: IDeleteUserModalProps) {
-    return (
-        <>
-            <Modal open={isVisible} onClose={handleClose}>
-                <Box sx={modalBoxStyle}>
-                    <div className="mb-8">
-                        <h1 className="font-bold text-xl">Kullanıcı Sil</h1>
-                    </div>
+function DeleteUserModal({
+  isVisible,
+  handleClose,
+  state,
+  userId,
+}: IDeleteUserModalProps) {
+  const handleEnterKeyPress = (event: React.KeyboardEvent) => {
+    if (event.key === "Enter") {
+      deleteUser(userId, state, handleClose);
+    }
+  };
 
-                    <div>
-                        <Button variant="contained" color="error" onClick={() => {
-                            deleteUser(userId, state, handleClose);
-                        }}>
-                            Kullanıcıyı Sil
-                        </Button>
-                    </div>
-                </Box>
-            </Modal>
-        </>
-    );
+  return (
+    <>
+      <Modal
+        onKeyDown={handleEnterKeyPress}
+        open={isVisible}
+        onClose={handleClose}
+      >
+        <Box sx={modalBoxStyle}>
+          <div className="mb-8">
+            <h1 className="font-bold text-xl">Kullanıcı Sil</h1>
+          </div>
+
+          <div>
+            <Button
+              variant="contained"
+              color="error"
+              onClick={() => {
+                deleteUser(userId, state, handleClose);
+              }}
+            >
+              Kullanıcıyı Sil
+            </Button>
+          </div>
+        </Box>
+      </Modal>
+    </>
+  );
 }
 
 export default DeleteUserModal;
